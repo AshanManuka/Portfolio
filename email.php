565a33380bb836878f190c49f80bc75431e46b78
Manuka <manukajayarathne.coma@gmail.com>
2022-09-05 18:34:58 +0530

    done e-mail function

--- email.php
+++ email.php
@@ -1,3 +1,30 @@
 <?php
+$subject = 'You Got Message From Portfolio';
+$to = 'manukajayarathne.coma@gmail.com';
+$emailTo = $_REQUEST['txtEmail'];
+
+$name = $_REQUEST['txtName'];
+$email = $_REQUEST['txtEmail'];
+$msg = $_REQUEST['txtMessage'];
+
+$email_from = $name.'<'.$email.'>';
+
+$headers = "MIME-Version: 1.1";
+$headers .= "Content-type: text/html; charset=iso-8859-1";
+$headers .= "From: ".$name.'<'.$email.'>'."\r\n"; // Sender's E-mail
+$headers .= "Return-Path:"."From:" . $email;
+
+$message .= 'Name : ' . $name . "\n";
+$message .= 'Email : ' . $email . "\n";
+$message .= 'Message : ' . $msg;
+
+if (@mail($to, $subject, $message, $email_from))
+{
+    echo 'sent';
+}
+else
+{
+    echo 'failed';
+}
 
 
