f1090df8df95c79b7d68f320644e863cce1b18fb
Manuka <manukajayarathne.coma@gmail.com>
2022-10-07 22:28:15 +0530

    created data arrays for store

---
+++ Assignments/javaScript/spa/db/Db.js
@@ -0,0 +1,9 @@
+
+// Customers data Array
+    var customerList = [];
+// Items data Array
+    var itemList = [];
+// Orders data Array
+    var orderList = [];
+// select Items when purchasing data Array
+    var selectedItemList = [];
