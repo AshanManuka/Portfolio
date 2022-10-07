884078c8d636799d8f3c2b20eefea93cd4fe4134
Manuka <manukajayarathne.coma@gmail.com>
2022-10-07 23:14:40 +0530

    done navigating text fields using enter key

--- Assignments/javaScript/spa/controllers/customerController.js
+++ Assignments/javaScript/spa/controllers/customerController.js
@@ -3,15 +3,36 @@
 var cId = /^(C)[0-9]{3}$/;
 var cName = /^[a-z ]{3,30}$/;
 var cAddress = /^[a-z0-9 ,]{5,40}$/;
-var cSalary = /^[1-9][0-9]*(.[0-9]{2})?$/
+var cSalary = /^[1-9][0-9]*(.[0-9]{2})?$/;
+
 
 // Disable key TAB to disable function move over textFields
-
 $("#txtCustomerID,#txtCustomerName,#txtCustomerAddress,#txtCustomerSalary").on('keydown',function(event){
     if(event.key === "Tab"){
        event.preventDefault();
-       alert(event.key);
     }
 });
 
+$("#txtCustomerID").on('keydown',function (event) {
+    if(event.key === "Enter"){
+        $("#txtCustomerName").focus();
+    }
+});
 
+$("#txtCustomerName").on('keydown',function (event) {
+    if(event.key === "Enter"){
+        $("#txtCustomerAddress").focus();
+    }
+});
+
+$("#txtCustomerAddress").on('keydown',function (event) {
+    if(event.key === "Enter"){
+        $("#txtCustomerSalary").focus();
+    }
+});
+
+$("#txtCustomerSalary").on('keydown',function (event) {
+    if(event.key === "Enter"){
+        $("#Add").focus();
+    }
+});
