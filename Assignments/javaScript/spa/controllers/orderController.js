fce470ce39d6edbcdd1d3bde3b0e7aed2d714992
Manuka <manukajayarathne.coma@gmail.com>
2022-10-09 22:06:43 +0530

    done adding customers to optionbox

--- Assignments/javaScript/spa/controllers/orderController.js
+++ Assignments/javaScript/spa/controllers/orderController.js
@@ -1,3 +1,15 @@
 
+$("#order-btn").on('click',function(){
+   loadCustomerList();
+});
+
+
+function loadCustomerList(){
+    $("#cust-id").empty();
+
+    for (cust of customerList) {
+        $("#cust-id").append(`<option>${cust.id}</option>`);
+    }
+}
 
 
