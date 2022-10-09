31d7a43f78e970c9934623a1018d153464355d90
Manuka <manukajayarathne.coma@gmail.com>
2022-10-09 23:01:39 +0530

    make function for selec data from dropdown box

--- Assignments/javaScript/spa/controllers/orderController.js
+++ Assignments/javaScript/spa/controllers/orderController.js
@@ -4,10 +4,13 @@ $("#order-btn").on('click',function(){
    loadItemList();
 });
 
+$('#cust-id').on('change', function() {
+    var valueId = $(this).val();
+    searchItemToFill(valueId);
+});
 
 function loadCustomerList(){
     $("#cust-id").empty();
-
     for (cust of customerList) {
         $("#cust-id").append(`<option>${cust.id}</option>`);
     }
@@ -15,10 +18,15 @@ function loadCustomerList(){
 
 function loadItemList(){
     $("#itm-name").empty();
-
     for (itm of itemList) {
         $("#itm-name").append(`<option>${itm.name}</option>`);
     }
 }
+
+function searchItemToFill(value){
+    alert(value);
+}
+
+
 
 
