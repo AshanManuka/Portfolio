5d5ca5d21852fc1d277d2a93aecd769d86a0fcf5
Manuka <manukajayarathne.coma@gmail.com>
2022-10-09 23:19:52 +0530

    done autofill function when selecting from box

--- Assignments/javaScript/spa/controllers/orderController.js
+++ Assignments/javaScript/spa/controllers/orderController.js
@@ -4,10 +4,20 @@ $("#order-btn").on('click',function(){
    loadItemList();
 });
 
+// select customer id from dropdown box
 $('#cust-id').on('change', function() {
+    var valueId = $(this).val();
+    searchCustomerToFill(valueId);
+});
+
+
+// select item name from dropdown box
+$('#itm-name').on('change', function() {
     var valueId = $(this).val();
     searchItemToFill(valueId);
 });
+
+
 
 function loadCustomerList(){
     $("#cust-id").empty();
@@ -23,10 +33,22 @@ function loadItemList(){
     }
 }
 
-function searchItemToFill(value){
+function searchCustomerToFill(value){
     for (cus of customerList) {
         if (cus.id === value){
             $("#txtCustName").val(cus.name);
+        }
+    }
+}
+
+
+function searchItemToFill(value){
+    for (itm of itemList) {
+        if (itm.name === value){
+            $("#itemId").val(itm.code);
+            $("#uniPrice").val(itm.price);
+            $("#qty").val(itm.qty);
+
         }
     }
 }
