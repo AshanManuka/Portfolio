89bd8495a01651ddf1b3124a0e555860acb34fc6
Manuka <manukajayarathne.coma@gmail.com>
2022-10-09 07:47:12 +0530

    added sweet alert to update function

--- Assignments/javaScript/spa/controllers/customerController.js
+++ Assignments/javaScript/spa/controllers/customerController.js
@@ -244,7 +244,24 @@ function deleteCustomer(){
 
 // customer update function
 $("#update").on('click',function(){
-   updateCustomer();
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, Update it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            updateCustomer();
+            Swal.fire(
+                'Updated!',
+                'Data has been Updated.',
+                'success'
+            )
+        }
+    })
 });
 
 function updateCustomer(){
