080f61307d1998d9be271c7bf00ad7540259bf3f
Manuka <manukajayarathne.coma@gmail.com>
2022-11-30 22:03:26 +0530

    enemies are flow to down

--- Assignments/javaScript/JS-Game/js/enemyController.js
+++ Assignments/javaScript/JS-Game/js/enemyController.js
@@ -1,13 +1,35 @@
-var n = 15;
+var one = 15;
+var two = 15;
+var three = 15;
 
-setInterval(flowEnemy,60);
+setInterval(flowEnemyOne,60);
+setInterval(flowEnemyTwo,50);
+setInterval(flowEnemyThree,45);
 
 function flowEnemyOne(){
-    goEnemy(n);
-    n += 5;
+    goEnemyOne(one);
+    one += 5;
 
-    if(n === 400){
-        n = 0;
+    if(one === 400){
+        one = 0;
+    }
+}
+
+function flowEnemyTwo(){
+    goEnemyTwo(two);
+    two += 5;
+
+    if(two === 400){
+        two = 0;
+    }
+}
+
+function flowEnemyThree(){
+    goEnemyThree(three);
+    three += 5;
+
+    if(three === 400){
+        three = 0;
     }
 }
 
@@ -15,6 +37,10 @@ function goEnemyOne(n){
     $("#enemy-one").css('top',n);
 }
 
-function goEnemyTwo(){
-    $("#enemy-one").css('top',n);
+function goEnemyTwo(n){
+    $("#enemy-two").css('top',n);
+}
+
+function goEnemyThree(n){
+    $("#enemy-three").css('top',n);
 }
